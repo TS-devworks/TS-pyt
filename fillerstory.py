6f51069f524a4dc9bc30e45a2f492473eac6c0c5
T. Smith <108356114+TS-devworks@users.noreply.github.com>
2022-08-29 20:03:47 -0400

    User Filled Mad Lib
    Made up story-line.

---
+++ fillerstory.py
@@ -0,0 +1,45 @@
+#User Filler Story 01
+#29 August 2022
+#Trista Smith
+
+print('''
+ -------------------------------
+|                              |
+|                              |
+|    U S E R  F I L L E R      |
+|        S T O R Y  #001       |
+|          By: T.Smith         |
+|                              |
+ -------------------------------   
+
+
+''')
+
+pronoun = input('Proper Noun: ')
+somethingtoread = input('Anything you can read: ')
+adj001 = input('Adjective 1: ')
+adj002 = input('Adjective 2: ')
+knockover = input('A way to knock one over: ')
+verb001 = input('Verb ending in "-ing": ')
+adj003 = input('Adjective 3: ')
+verb002 = input('Verb ending in -ed: ')
+moviename = input('Name of a movie: ')
+verb = input('Something to do ending in -ing: ')
+noun = input('Enter a noun: ')
+
+text = f'''
+
+Mr.{pronoun} read the {somethingtoread} end to end, 
+and as he read he got a sense {adj001}, 
+{adj002} energy surrounding him. 
+It was able to {knockover} effortlessly, nearly 
+{verb001} him to death. 
+Imagine being in Mr.{pronoun}s postion. 
+He's {adj003} and {verb002} in the
+corner of the room. That looks like a scene right out of {moviename}. 
+So I guess we just {verb} while he does his thing. 
+
+Hey filler want to go {noun}? Cool, let's go! 
+
+'''
+print(text)
